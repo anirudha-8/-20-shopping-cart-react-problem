1c35970bcf5817f72be49841792a6f88f202c371
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-21 11:40:30 +0530

    add different products

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-	return ();
+	const products = [
+		{ id: 1, name: "Product A" },
+		{ id: 2, name: "Product B" },
+		{ id: 3, name: "Product C" },
+		{ id: 4, name: "Product D" },
+	];
+	return "Shopping Cart!";
 }
 
 export default App;
