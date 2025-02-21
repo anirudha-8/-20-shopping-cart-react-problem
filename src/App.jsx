baf082e4c3a94f9a92c8eda879e93eb8f3b751c2
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-21 11:44:23 +0530

    display list of products

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ function App() {
 		{ id: 3, name: "Product C" },
 		{ id: 4, name: "Product D" },
 	];
-	return "Shopping Cart!";
+	return (
+		<div>
+			<h1>Shopping Cart</h1>
+			<h2>Products</h2>
+			<ul>
+				{products.map((product) => {
+					return (
+						<li key={product.id}>
+							{product.name} <button>Add To Cart</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
